75807e686a0c313e07a3500fe94cc3f431bba82c
sertanmete <sertanmete@gmail.com>
2022-04-11 23:00:05 +0200

    12yr3

--- src/test/java/pack/class1.java
+++ src/test/java/pack/class1.java
@@ -9,5 +9,6 @@ public class class1 {
         System.out.println("yine ben");
         System.out.println("sertan");
         System.out.println("master");
+        System.out.println("sezer");
     }
 }
